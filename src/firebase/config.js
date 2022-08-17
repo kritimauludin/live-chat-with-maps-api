4978c17b7b99a712881ca4454de43924755bf072
Kriti Mauludin <49935056+kritimauludin@users.noreply.github.com>
2022-08-17 20:29:01 +0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import firestore from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBYYxwTH7UOavholr4Qxs0f4QYnVWA7KrQ",
-    authDomain: "learnvue-location.firebaseapp.com",
-    projectId: "learnvue-location",
-    storageBucket: "learnvue-location.appspot.com",
-    messagingSenderId: "649318230254",
-    appId: "1:649318230254:web:f899068687e23432722bff",
-    measurementId: "G-RZ0MLX2XCN"
+    apiKey: "your-api-key",
+    authDomain: "your-auth-domain,
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "...",
+    measurementId: "..."
   };
 
   const app = firebase.initializeApp(firebaseConfig)
